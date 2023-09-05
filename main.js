eecbd43aeabd12d599e564767358df16656c7f53
Alex-G-R <alex.grabski555@gmail.com>
2023-09-05 21:00:52 +0200

    Setting up the packages and files

---
+++ main.js
@@ -0,0 +1,15 @@
+const PORT = 8000;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+const colors = require('colors');
+
+const app = express();
+
+const startScraping = async () => {
+
+}
+
+app.listen(PORT, ()=>console.log(`Server is starting on port ${PORT}`))
+
+
